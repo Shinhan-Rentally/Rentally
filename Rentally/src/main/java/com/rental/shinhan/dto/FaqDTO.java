ca7c55b9a0a675fd8f6502df1532c2f51dda6500
ParkEunsoo <dmstn_0526@naver.com>
2024-12-17 19:31:48 +0900

    [feat]: faqDTO 개발(#6)

---
+++ Rentally/src/main/java/com/rental/shinhan/dto/FaqDTO.java
@@ -0,0 +1,17 @@
+package com.rental.shinhan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class FaqDTO {
+	int faq_seq;
+	String faq_question;
+	String faq_answer;
+	int admin_seq;
+}
