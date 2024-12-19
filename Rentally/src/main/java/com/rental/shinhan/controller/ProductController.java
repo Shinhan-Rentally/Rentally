4f15be97cda0594839230178a6c8a8a6a0771c63
jeongdung-eo <jeuniis2@gmail.com>
2024-12-19 15:09:46 +0900

    [feat]: productController 개발(#7)

---
+++ Rentally/src/main/java/com/rental/shinhan/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.rental.shinhan.controller;
+
+import com.rental.shinhan.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/product/list")
+    public String selectAllProducts(Model model) {
+        model.addAttribute("products", productService.selectAllProducts());
+        return "product/list";
+    }
+}
