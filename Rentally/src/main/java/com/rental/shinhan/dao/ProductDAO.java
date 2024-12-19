b6b7fb7dd7f82e397f699dadb0246c682db16c77
jeongdung-eo <jeuniis2@gmail.com>
2024-12-20 00:51:16 +0900

    [fix]: 불필요한 코드 삭제(#7)

--- Rentally/src/main/java/com/rental/shinhan/dao/ProductDAO.java
+++ Rentally/src/main/java/com/rental/shinhan/dao/ProductDAO.java
@@ -2,14 +2,12 @@ package com.rental.shinhan.dao;
 
 import com.rental.shinhan.daointerface.ProductInterface;
 import com.rental.shinhan.dto.ProductDTO;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Slf4j
 @Repository
 public class ProductDAO implements ProductInterface {
 
@@ -20,7 +18,6 @@ public class ProductDAO implements ProductInterface {
 
     public List<ProductDTO> selectAllProducts() {
         List<ProductDTO> products = sqlSession.selectList(namespace + "selectAll");
-        log.info(products.toString());
         return products;
     }
 }
