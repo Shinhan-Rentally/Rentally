e173d525ad499da0424d01c99e5e6758dedeb6c2
Jeong Jaeyoon <jyoon3741@naver.com>
2024-12-19 12:40:33 +0900

    [feat]:CustomerDTO 개발(#20)

---
+++ Rentally/src/main/java/com/rental/shinhan/dto/CustomerDTO.java
@@ -0,0 +1,22 @@
+package com.rental.shinhan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+	int cust_seq;
+	String cust_id;
+	String cust_pw;
+	String cust_phone;
+	String cust_email;
+	String cust_name;
+	boolean cust_status;
+}
