6fc08943f509a014b20233d2a05493380b9575d6
jeongdung-eo <jeuniis2@gmail.com>
2024-12-21 00:14:27 +0900

    [feat]: controller에 delete 로직 개발(#39)

--- Rentally/src/main/java/com/rental/shinhan/controller/AdminController.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/AdminController.java
@@ -2,12 +2,10 @@ package com.rental.shinhan.controller;
 
 import com.rental.shinhan.dto.ProductDTO;
 import com.rental.shinhan.dto.ReviewDTO;
-
 import com.rental.shinhan.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +15,8 @@ public class AdminController {
 
     @Autowired
     AdminService adminService;
+
+    private int productSeq = 1;
 
     @GetMapping("/product/list")
     public String getProducts() {
@@ -29,6 +29,12 @@ public class AdminController {
     public String getReviews() {
       
         List<ReviewDTO> reviews = adminService.findReviews();
+        return "";
+    }
+
+    @PostMapping("/{productSeq}/delete")
+    public String deleteProduct(@PathVariable int productSeq){
+        int result = adminService.removeProduct(productSeq);
         return "";
     }
 }
