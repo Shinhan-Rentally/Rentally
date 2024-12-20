14b71bbca80165a2d60a8ce9e56e695a96e172f8
jeongdung-eo <jeuniis2@gmail.com>
2024-12-20 10:39:21 +0900

    [feat]: Dao에 review로직 개발(#23)

--- Rentally/src/main/java/com/rental/shinhan/dao/AdminDAO.java
+++ Rentally/src/main/java/com/rental/shinhan/dao/AdminDAO.java
@@ -2,6 +2,7 @@ package com.rental.shinhan.dao;
 
 import com.rental.shinhan.daointerface.AdminInterface;
 import com.rental.shinhan.dto.ProductDTO;
+import com.rental.shinhan.dto.ReviewDTO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,8 +17,13 @@ public class AdminDAO implements AdminInterface {
     @Autowired
     SqlSession sqlSession;
 
-    public List<ProductDTO> selectAllProducts() {
+    public List<ProductDTO> selectProducts() {
         List<ProductDTO> products = sqlSession.selectList(namespace + "selectProducts");
         return products;
+    }
+
+    public List<ReviewDTO> selectReviews() {
+        List<ReviewDTO> reviews = sqlSession.selectList(namespace + "selectReviews");
+        return reviews;
     }
 }
--- Rentally/src/main/java/com/rental/shinhan/daointerface/AdminInterface.java
+++ Rentally/src/main/java/com/rental/shinhan/daointerface/AdminInterface.java
@@ -1,10 +1,12 @@
 package com.rental.shinhan.daointerface;
 
 import com.rental.shinhan.dto.ProductDTO;
+import com.rental.shinhan.dto.ReviewDTO;
 
 import java.util.List;
 
 public interface AdminInterface {
 
-    public List<ProductDTO> selectAllProducts();
+    public List<ProductDTO> selectProducts();
+    public List<ReviewDTO> selectReviews();
 }
