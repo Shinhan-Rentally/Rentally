8cac61e13f6edfc7c107ef1abf33c3973cf4c5ca
dmstn_0526 <dmstn_0526@naver.com>
2024-12-23 12:23:23 +0900

    [feat]: customer 회원정보 수정 서비스 개발 (#61)

--- Rentally/src/main/java/com/rental/shinhan/service/CustomerService.java
+++ Rentally/src/main/java/com/rental/shinhan/service/CustomerService.java
@@ -1,10 +1,7 @@
 package com.rental.shinhan.service;
 
-import com.rental.shinhan.dao.AdminDAO;
 import com.rental.shinhan.dao.CustomerDAO;
 import com.rental.shinhan.dto.CustomerDTO;
-import com.rental.shinhan.dto.ProductDTO;
-import com.rental.shinhan.dto.ReviewDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +19,9 @@ public class CustomerService {
 
     public int deleteCustomer(int cust_seq) {
         return custDAO.deleteCustomer(cust_seq);
+    }
+
+    public int updateCustInfo(CustomerDTO cust) {
+        return custDAO.updateCustInfo(cust);
     }
 }
