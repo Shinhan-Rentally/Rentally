e61c5232e83115b6a2c576e753f5d98933916497
Jeong Jaeyoon <jyoon3741@naver.com>
2024-12-23 09:43:01 +0900

    [feat]:cartDAO 수정(#31)

--- Rentally/src/main/java/com/rental/shinhan/dao/CartDAO.java
+++ Rentally/src/main/java/com/rental/shinhan/dao/CartDAO.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.rental.shinhan.dto.CartJoinDTO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class CartDAO {
 	@Autowired
 	SqlSession sqlSession;
@@ -14,6 +17,7 @@ public class CartDAO {
 	String namespace = "com.rental.shinhan.";
 	public List<CartJoinDTO> selectCart(int cust_id){
 		List<CartJoinDTO> cartlist = sqlSession.selectList(namespace+"selectCart", cust_id);
+		log.info(cartlist.size()+"°³ Á¶È¸");
 		return cartlist;
 	}
 }
