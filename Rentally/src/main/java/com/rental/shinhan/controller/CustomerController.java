ffbaffba141c5f18a8d93d809bcf0b0b87272f8f
dmstn_0526 <dmstn_0526@naver.com>
2024-12-23 12:27:20 +0900

    [fix]: customer 회원정보 수정 컨트롤러 수정 (#61)

--- Rentally/src/main/java/com/rental/shinhan/controller/CustomerController.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/CustomerController.java
@@ -2,16 +2,15 @@ package com.rental.shinhan.controller;
 
 import com.rental.shinhan.dto.CustomerDTO;
 import com.rental.shinhan.service.CustomerService;
-import com.rental.shinhan.util.ApiResponse;
-import com.rental.shinhan.util.SuccessType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
@@ -32,17 +31,12 @@ public class CustomerController {
     }
 
     @PostMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ApiResponse<Integer> updateCustInfo(
+    public String updateCustInfo(
             @RequestBody CustomerDTO custInfo,
             HttpSession session) {
         custInfo.setCust_seq(cust_seq);
         int result = custService.updateCustInfo(custInfo);
-        return ApiResponse.success(SuccessType.SUCCESS, result);
+        return "";
     }
-//    @PostMapping(value="/add",consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ApiResponse<Integer> createWishList(@RequestBody final WishListDTO request) {
-//        int result = wishListService.addWishList(request);
-//        return ApiResponse.success(SuccessType.SUCCESS, result);
-//    }
 
 }
