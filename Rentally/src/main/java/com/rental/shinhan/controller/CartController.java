f02d85b73ffb4ee1d2ed224ac1843dbfefa593a4
Jeong Jaeyoon <jyoon3741@naver.com>
2024-12-23 09:56:00 +0900

    [feat]:CartController 생성(#31)

---
+++ Rentally/src/main/java/com/rental/shinhan/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.rental.shinhan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rental.shinhan.service.CartService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class CartController {
+	@Autowired
+	CartService cartService;
+	
+	@GetMapping("/cart/list")
+	public String cartList(int cust_id, Model model) {
+		model.addAttribute("cartList", cartService.selectCart(cust_id));
+		//장바구니 jsp 페이지
+		return "";
+	}
+	
+}
