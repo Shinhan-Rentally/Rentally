31557ab21f0a9870c21fc3a54f547205798907d4
Jeong Jaeyoon <jyoon3741@naver.com>
2024-12-26 11:07:29 +0900

    [fix]:dao에 이메일합침 추가(#103)

--- Rentally/src/main/java/com/rental/shinhan/dao/JoinDAO.java
+++ Rentally/src/main/java/com/rental/shinhan/dao/JoinDAO.java
@@ -18,9 +18,18 @@ public class JoinDAO {
 	String namespace = "com.rental.shinhan.customer.";
 	
 	public int insert(CustomerDTO cust) {
+		//cust_email과 cust_email2합쳐서 cust_email로 저장
+		String fullEmail = cust.getCust_email() + cust.getCust_email2();
+		cust.setCust_email(fullEmail);
+		//비밀번호 해싱
 		String hashedPw = BCrypt.withDefaults().hashToString(12, cust.getCust_pw().toCharArray());
 		cust.setCust_pw(hashedPw);
 		int result = sqlSession.insert(namespace + "insertCustomer", cust);
+		if(result>0) {
+			log.info("회원가입 성공 >>"+cust.toString());
+		} else {
+			log.info("회원가입 실패");
+		}
 		return result;
 	}
 }
