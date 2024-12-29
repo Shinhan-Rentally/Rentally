89dc5c2dd3e7c013a2d5864be8fb96c93320821e
jeongdung-eo <jeuniis2@gmail.com>
2024-12-30 04:10:06 +0900

    [feat]: 사용자 조회 페이지네이션 로직 구현(#204)

--- Rentally/src/main/java/com/rental/shinhan/controller/AdminController.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/AdminController.java
@@ -7,6 +7,7 @@ import com.rental.shinhan.dto.ReviewDTO;
 import com.rental.shinhan.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,9 +36,23 @@ public class AdminController {
     }
 
     @GetMapping("/customer/list")
-    public String getCustomers() {
+    public String getCustomers(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            Model model
+    ) {
         List<CustomerDTO> customers = adminService.findCustomers();
-        return "";
+        int start = (page - 1) * size;
+        int end = Math.min(start + size, customers.size());
+
+        List<CustomerDTO> pagedCustomers = customers.subList(start, end);
+        int totalPages = (int) Math.ceil((double) customers.size() / size);
+
+        model.addAttribute("customers", pagedCustomers);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("totalOrders", customers.size());
+        model.addAttribute("currentPage", page);
+        return "admin/customers";
     }
 
     @GetMapping("/order/list")
