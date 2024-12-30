316ae7f1061419227ddc2ce2f5afef5133bb55d3
jeongdung-eo <jeuniis2@gmail.com>
2024-12-31 01:53:01 +0900

    [feat]: service에 상품 등록 로직 추가(#224)

--- Rentally/src/main/java/com/rental/shinhan/service/AdminService.java
+++ Rentally/src/main/java/com/rental/shinhan/service/AdminService.java
@@ -1,13 +1,16 @@
 package com.rental.shinhan.service;
 
 import com.rental.shinhan.dao.AdminDAO;
-import com.rental.shinhan.dto.OrderJoinDTO;
 import com.rental.shinhan.dto.CustomerDTO;
+import com.rental.shinhan.dto.OrderJoinDTO;
 import com.rental.shinhan.dto.ProductDTO;
 import com.rental.shinhan.dto.ReviewDTO;
+import com.rental.shinhan.util.S3Uploader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -22,7 +25,7 @@ public class AdminService {
     }
 
     public List<ReviewDTO> findReviews() {
-      
+
         return adminDAO.selectReviews();
     }
 
@@ -38,5 +41,26 @@ public class AdminService {
 
     public int removeProduct(int productSeq) {
         return adminDAO.deleteProduct(productSeq);
+    }
+
+    @Autowired
+    S3Uploader s3Uploader;
+
+    public int addProduct(List<MultipartFile> images, ProductDTO product) throws IOException {
+        try {
+            if (!images.isEmpty()) {
+                String categorySeq = String.valueOf(product.getCategory_seq());
+                // S3에 이미지 업로드
+                String imgFile = s3Uploader.upload(images.get(0), categorySeq);
+                String detailFile = s3Uploader.upload(images.get(1), categorySeq);
+                // DTO에 저장된 URL 설정
+                product.setProduct_img(imgFile);
+                product.setProduct_detail(detailFile);
+            }
+            return adminDAO.insertProduct(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
