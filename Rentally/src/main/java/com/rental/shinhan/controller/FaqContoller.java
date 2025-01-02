e3f11281ec7e1a6593cbe211c3b24315309e0979
dmstn0526 <dmstn_0526@naver.com>
2025-01-02 17:37:02 +0900

    [fix]: 문의하기 수정 및 개발(#305)

--- Rentally/src/main/java/com/rental/shinhan/controller/FaqContoller.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/FaqContoller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,5 +20,9 @@ public class FaqContoller {
         List<FaqDTO> faqlist = fService.selectService();
         model.addAttribute("faqlist", faqlist);
         return "/faq/faqlist"; //jsp 파일명 정해서 넣기
+    }
+    @GetMapping("/faq/contact")
+    public String contactUs() {
+        return "/faq/contactUs";
     }
 }
