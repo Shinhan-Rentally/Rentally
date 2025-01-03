4517bbecfb1523e2ce9788482d1587909ce1437f
dmstn0526 <dmstn_0526@naver.com>
2025-01-03 15:11:33 +0900

    [fix]: 주문내역조회 값 수정 (#329)

--- Rentally/src/main/java/com/rental/shinhan/controller/OrderController.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/OrderController.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -23,6 +24,15 @@ public class OrderController {
     public String getOrders(HttpSession seesion, Model model) {
         int custSeq = (int) seesion.getAttribute("cust_seq");
         List<SubscribeListJoinDTO> orders = orderService.findOrders(custSeq);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        orders.forEach(order -> {
+            Date paydate = order.getSub_paydate();
+            if (paydate != null) {
+                order.setFormatted_paydate(dateFormat.format(paydate)); // 포맷된 날짜를 추가
+            }
+        });
+
         model.addAttribute("myOrders",orders);
         System.out.println(orders);
         return "/order/orderList";
