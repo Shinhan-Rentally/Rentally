72762f6a2f6c9734be4335c81f3ebcc5eb215af3
dmstn0526 <dmstn_0526@naver.com>
2025-01-10 16:40:01 +0900

    [fix]: import문 정리 (#453)

--- Rentally/src/main/java/com/rental/shinhan/controller/OrderController.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.rental.shinhan.dto.SubscribeListJoinDTO;
 import com.rental.shinhan.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/order")
