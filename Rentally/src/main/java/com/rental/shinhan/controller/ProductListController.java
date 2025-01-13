10b7960bc49d28a01d172fa0730eb5a20e8fe5b5
Jeong Jaeyoon <jyoon3741@naver.com>
2025-01-13 11:19:49 +0900

    [fix]: controller - 코드컨벤션 수정(#458)

--- Rentally/src/main/java/com/rental/shinhan/controller/ProductListController.java
+++ Rentally/src/main/java/com/rental/shinhan/controller/ProductListController.java
@@ -123,7 +123,7 @@ public class ProductListController {
 	ReviewService reviewService;
 
 	@GetMapping("/detail")
-	public String productDetail(HttpSession session, int product_seq, Model model) {
+	public String productDetail(HttpSession session, @RequestParam("product_seq") int productSeq, Model model) {
 		Integer custSeq = (Integer) session.getAttribute("cust_seq");
 
 		// 로그인 여부에 따라 위시리스트 처리
@@ -132,8 +132,8 @@ public class ProductListController {
 			wishlist = wishlistService.wishStatus(custSeq);
 		}
 
-		model.addAttribute("detail", productlistService.selectProductDetail(product_seq));
-		model.addAttribute("reviewList", reviewService.selectReview(product_seq));
+		model.addAttribute("detail", productlistService.selectProductDetail(productSeq));
+		model.addAttribute("reviewList", reviewService.selectReview(productSeq));
 		model.addAttribute("wishlist", wishlist);
 		return "product/detail";
 	}
